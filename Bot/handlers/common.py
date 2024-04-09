108fa788fa96456b56946f95ebc06ca05739dab5
Vitaliy Girenok <girenok10@mail.ru>
2024-04-09 17:58:01 +0500

    divided into directories

---
+++ Bot/handlers/common.py
@@ -0,0 +1,55 @@
+from aiogram import types, F, Router
+from aiogram.filters.command import Command
+import logging
+from Bot.Keyboards.Keyboards import kb2
+from Bot.Utils.RandomFox import fox
+
+router = Router()
+
+@router.message(Command('start'))
+async def cmd_start(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Привет {name}')
+
+
+@router.message(Command('stop'))
+async def cmd_stop(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Пока {name}')
+
+
+@router.message(Command('info'))
+async def cmd_info(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Этот бот создан для тренировки навыков и не несет существенной информации')
+
+
+@router.message(Command('user'))
+async def cmd_user(message: types.Message):
+    name = message.chat.first_name
+    await message.answer(f'Пользователь - {name}')
+
+
+
+@router.message(Command('fox'))
+@router.message(Command('лиса'))
+@router.message(F.text.lower() == 'покажи лису')
+async def cmd_fox(message: types.Message):
+    name = message.chat.first_name
+    img_fox = fox()
+    await message.answer(f'{name} держи лису')
+    await message.answer_photo(photo=img_fox)
+
+
+@router.message(F.text)
+async def cmd_echo(message: types.Message):
+    msg_user = message.text.lower()
+    name = message.chat.first_name
+    if "привет" in msg_user:
+        await message.answer(f'Привет-привет, {name}')
+    elif "что ты умеешь делать" in msg_user:
+        await message.answer("я ничего не умею, я еще не умный")
+    elif "лиса" in msg_user:
+        await message.answer(f'смотри что у меня есть, {name}', reply_markup=kb2)
+    else:
+        await message.answer(f'Ты написал - {msg_user}')
--- Bot/main.py
+++ Bot/main.py
@@ -1,66 +1,15 @@
 import asyncio
 import config
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters.command import Command
 import logging
-from Keyboards import kb2
-from RandomFox import fox
-
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=config.token)
-dp = Dispatcher()
-
-@dp.message(Command('start'))
-async def cmd_start(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f'Привет {name}')
-
-
-@dp.message(Command('stop'))
-async def cmd_stop(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f'Пока {name}')
-
-
-@dp.message(Command('info'))
-async def cmd_info(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f'Этот бот создан для тренировки навыков и не несет существенной информации')
-
-
-@dp.message(Command('user'))
-async def cmd_user(message: types.Message):
-    name = message.chat.first_name
-    await message.answer(f'Пользователь - {name}')
-
-
-
-@dp.message(Command('fox'))
-@dp.message(Command('лиса'))
-@dp.message(F.text.lower() == 'покажи лису')
-async def cmd_fox(message: types.Message):
-    name = message.chat.first_name
-    img_fox = fox()
-    await message.answer(f'{name} держи лису')
-    await message.answer_photo(photo=img_fox)
-
-
-@dp.message(F.text)
-async def cmd_echo(message: types.Message):
-    msg_user = message.text.lower()
-    name = message.chat.first_name
-    if "привет" in msg_user:
-        await message.answer(f'Привет-привет, {name}')
-    elif "что ты умеешь делать" in msg_user:
-        await message.answer("я ничего не умею, я еще не умный")
-    elif "лиса" in msg_user:
-        await message.answer(f'смотри что у меня есть, {name}', reply_markup=kb2)
-    else:
-        await message.answer(f'Ты написал - {msg_user}')
-
+from handlers import common
 
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=config.token)
+    dp = Dispatcher()
+    dp.include_router(common.router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
